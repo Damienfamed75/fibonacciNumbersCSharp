ff6d44b4bc829f90ccbca2e222f2ca52737344fc
Damien Stamates <thedoubledimensions@gmail.com>
2017-09-01 00:49:10 -0500

    Added comments and refactored small bits
    Refactored small bit within the example and added comments across the file.

--- fibonacciNums/fibonacciNums/example.cs
+++ fibonacciNums/fibonacciNums/example.cs
@@ -26,30 +26,30 @@ namespace fibonacciNums
 
         public static void Main()
         {
-            while(true)
+            while(true) // repeats forever
             {
                 Console.Clear();
-                DetermineNumber();
-                Console.Write($"What's the {numString} number of the fibonacci sequence?\n\n> ");
-                string result = Console.ReadLine();
-                if (Int32.TryParse(result, out x))
+                DetermineNumber(); // chooses random fibonacci number to ask
+                Console.Write($"What's the {numString} number of the fibonacci sequence?\n\n> "); // Asks question
+                string result = Console.ReadLine(); // Stores result into string
+                if (Int32.TryParse(result, out x))  // Parses into integer 
                 {
-                    if (x == fiboCalc(numFib))
-                    {
-                        Console.Write($"You're right!! You answered: {result} : : Answer is: {fiboCalc(numFib).ToString()}\n\n<Press Any Key to Restart>");
-                    }
-                    else
-                    {
-                        Console.Write($"You're wrong!! You answered: {result} : : Answer is: {fiboCalc(numFib).ToString()}\n\n<Press Any Key to Restart>");
-                    }
+                    if (x == fiboCalc(numFib)) Console.Write("You're right!!"); // Checks if answer is correct
+                    else Console.Write("You're wrong!!");                       // Or incorrect
+
+                    Console.Write($" You answered: {result} : : Answer is: {fiboCalc(numFib).ToString()}");
+                } else 
+                {
+                    Console.Write("What you inputted was not a number. Try again");
                 }
+                Console.Write("\n\n<Press Any Key to Restart>");
                 Console.ReadKey();
             }
         }
         static void DetermineNumber ()
         {
             Random numGen = new Random();
-            switch (numGen.Next(5, 11))
+            switch (numGen.Next(5, 11)) // New number picker between 5 and 11
             {
                 case 5:
                     numString = "fifth";
@@ -75,7 +75,7 @@ namespace fibonacciNums
                     numString = "tenth";
                     numFib = 10;
                     break;
-                default:
+                default:    // if it messes up
                     numString = "eleventh";
                     numFib = 10;
                     break;
